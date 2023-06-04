8c293e24da1d78ff0cc8e4f0d25c6f38a4463037
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2023-06-04 14:04:04 +0100

    Added bold to field

--- Dropdown/index.svelte
+++ Dropdown/index.svelte
@@ -1,222 +1,252 @@
 <script context="module">
-	const dropdowns={}
+  const dropdowns = {};
 
-	export function getDropdown(id=''){
-		return dropdowns[id]
-	}
+  export function getDropdown(id = "") {
+    return dropdowns[id];
+  }
 </script>
 
 <script>
-	import {onDestroy} from 'svelte'
+  import { onDestroy } from "svelte";
 
-	export let showMenu = false;
-	export let id = ''
-	export let style
-	export let fill
-	export let expandOnHover
-	export let showBorder
+  export let showMenu = false;
+  export let id = "";
+  export let style;
+  export let fill;
+  export let expandOnHover;
+  export let showBorder;
 
-	function clickOutside(element, callbackFunction) {
+  function clickOutside(element, callbackFunction) {
+    function onClick(event) {
+      if (!element.contains(event.target)) {
+        callbackFunction(event, element);
+      }
+    }
 
-		function onClick(event) {
-			if (!element.contains(event.target)) {
-				callbackFunction(event, element);
-			}
-		}
+    document.body.addEventListener("click", onClick);
 
-		document.body.addEventListener('click', onClick);
+    return {
+      update(newCallbackFunction) {
+        callbackFunction = newCallbackFunction;
+      },
+      destroy() {
+        document.body.removeEventListener("click", onClick);
+      },
+    };
+  }
 
-		return {
-			update(newCallbackFunction) {
-				callbackFunction = newCallbackFunction;
-			},
-			destroy() {
-				document.body.removeEventListener('click', onClick);
-			}
-		}
-	}
+  function close() {
+    showMenu = false;
+  }
 
-	function close() {
-		showMenu = false
-	}
+  function open() {
+    showMenu = true;
+  }
 
-	function open() {
-		showMenu = true
-	}
+  dropdowns[id] = { open, close };
 
-	dropdowns[id]={open, close}
-
-	onDestroy(()=>{
-		delete dropdowns[id]
-	})
-
-
+  onDestroy(() => {
+    delete dropdowns[id];
+  });
 </script>
 
-<div style={style} class="host Select {showMenu ? 'show' : ''} {fill ? 'fill' : ''} {expandOnHover ? 'expandOnHover' : ''} {showBorder ? 'showBorder': ''}" use:clickOutside={(event, element) => {
-	close()
-}}>
-	{#if $$slots.hitThing}
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div on:click={(event) => {
-		if (showMenu === false) {
-			open()
-		}
-		else {
-			close()
-		}
+<div
+  {style}
+  class="host Select {showMenu ? 'show' : ''} {fill
+    ? 'fill'
+    : ''} {expandOnHover ? 'expandOnHover' : ''} {showBorder
+    ? 'showBorder'
+    : ''}"
+  use:clickOutside={(event, element) => {
+    close();
+  }}
+>
+  {#if $$slots.hitThing}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+      on:click={(event) => {
+        if (showMenu === false) {
+          open();
+        } else {
+          close();
+        }
 
-		window.addEventListener("blur", () => {
-			// parentElement.classList.remove("show")
-			close()
-		});
-}}><slot name="hitThing" /></div>
-	{/if}
+        window.addEventListener("blur", () => {
+          // parentElement.classList.remove("show")
+          close();
+        });
+      }}
+    >
+      <slot name="hitThing" />
+    </div>
+  {/if}
 
-	<!-- {#if $$slots.icon}<span class="icon"><slot name="icon"/></span>{/if} -->
-	{#if $$slots.label}
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<div class="label" on:click={(event) => {
-			if (showMenu === false) {
-				open()
-			}
-			else {
-				close()
-			}
+  <!-- {#if $$slots.icon}<span class="icon"><slot name="icon"/></span>{/if} -->
+  {#if $$slots.label}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+      class="label"
+      on:click={(event) => {
+        if (showMenu === false) {
+          open();
+        } else {
+          close();
+        }
 
-			window.addEventListener("blur", () => {
-				// parentElement.classList.remove("show")
-				close()
-			});
-	}}>
-			<span class="text"><slot name="label"/></span>
-			
-			<span class="" style="margin-left: 8px">
-				<div class="icon figma-light">
-					<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path opacity="0.30" d="M1 2.5L4 5.5L7 2.5" stroke="black"/>
-						</svg>
-				</div>
-				<div class="icon figma-dark">
-					<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path opacity="0.30" d="M1 2.5L4 5.5L7 2.5" stroke="white"/>
-						</svg>
-					</div>
-			</span>
+        window.addEventListener("blur", () => {
+          // parentElement.classList.remove("show")
+          close();
+        });
+      }}
+    >
+      <span class="text"><slot name="label" /></span>
 
-		</div>
-	{/if}
-	<div class="content">
-		<slot name="content" />
-	</div>
-
-
+      <span class="" style="margin-left: 8px">
+        <div class="icon figma-light">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path opacity="0.30" d="M1 2.5L4 5.5L7 2.5" stroke="black" />
+          </svg>
+        </div>
+        <div class="icon figma-dark">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path opacity="0.30" d="M1 2.5L4 5.5L7 2.5" stroke="white" />
+          </svg>
+        </div>
+      </span>
+    </div>
+  {/if}
+  <div class="content">
+    <slot name="content" />
+  </div>
 </div>
 
 <style>
-
-	.Select > .label {
-		/* line-height: 1; */
-		border: 2px solid transparent;
-		place-items: center;
-		min-height: 30px;
-		/* margin-left: calc(
+  .Select {
+    padding: 1px;
+  }
+  .Select > .label {
+    /* line-height: 1; */
+    border: 2px solid transparent;
+    place-items: center;
+    min-height: 30px;
+    /* margin-left: calc(
 			var(--fgp-gap_item_column, 0px) + (-1 * var(--margin-100))
 		); */
-		/* margin-right: calc((-1 * var(--margin-100))); */
-		padding-inline: calc(var(--padding-100) - 2px);
-		border-radius: var(--border-radius-25);
-		position: relative;
-		display: flex; place-items: center;
-		/* min-height: 30px; */
-		cursor: default;
-	}
+    /* margin-right: calc((-1 * var(--margin-100))); */
+    padding-inline: calc(var(--padding-100) - 2px);
+    border-radius: var(--border-radius-25);
+    position: relative;
+    display: flex;
+    place-items: center;
+    /* min-height: 30px; */
+    cursor: default;
+  }
 
-	.Select:hover > .label {
-		/* padding-top: 1px; */
-		border-color: var(--figma-color-border, var(--color-black-10));
-		border-width: 1px;
-		padding-inline: calc(var(--padding-100) - 1px);
-	}
+  .Select:hover svg path {
+    opacity: 0.8;
+  }
 
-	.Select.showBorder .label {
-		/* padding-top: 1px; */
-		border-color: var(--figma-color-border, var(--color-black-10));
-		border-width: 1px;
-		padding-inline: calc(var(--padding-100) - 1px);
-	}
+  .Select.show svg path {
+    opacity: 0.8;
+  }
 
-	.Select > .label > .icon:first-child {
-		margin-left: calc((-1 * var(--margin-50)));
-		margin-right: var(--margin-25);
-	}
+  .Select:hover > .label {
+    /* padding-top: 1px; */
+    border-color: var(--figma-color-border, var(--color-black-10));
+    border-width: 1px;
+    padding-inline: calc(var(--padding-100) - 1px);
+  }
 
-	.Select.show > .label {
-		border-color: var(--figma-color-border, var(--color-black-10));
-		border-width: 1px;
-		padding-inline: calc(var(--padding-100) - 1px);
-	}
+  .Select.showBorder .label {
+    /* padding-top: 1px; */
+    border-color: var(--figma-color-border, var(--color-black-10));
+    border-width: 1px;
+    padding-inline: calc(var(--padding-100) - 1px);
+  }
 
-	/* .Select:not(.fill) > .label {
+  .Select > .label > .icon:first-child {
+    margin-left: calc((-1 * var(--margin-50)));
+    margin-right: var(--margin-25);
+  }
+
+  .Select.show > .label {
+    border-color: var(--figma-color-border, var(--color-black-10));
+    border-width: 1px;
+    padding-inline: calc(var(--padding-100) - 1px);
+  }
+
+  /* .Select:not(.fill) > .label {
 		max-width: 120px;
 	} */
 
-	.Select:not(.fill) > .label > span {
-		white-space: nowrap;
-    	overflow: hidden;
-    	text-overflow: ellipsis;
-	}
+  .Select:not(.fill) > .label > span {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 
-	.Select.fill {
-		flex-grow: 1;
-	}
+  .Select.fill {
+    flex-grow: 1;
+  }
 
-	.Select.fill > .label {
-		margin-right: 0;
-	}
+  .Select.fill > .label {
+    margin-right: 0;
+  }
 
-	.Select.fill:hover > .label > [icon="chevron-down"] {
-		margin-left: auto !important;
-	}
+  .Select.fill:hover > .label > [icon="chevron-down"] {
+    margin-left: auto !important;
+  }
 
-	.Select.fill.show > .label > [icon="chevron-down"] {
-		margin-left: auto !important;
-	}
+  .Select.fill.show > .label > [icon="chevron-down"] {
+    margin-left: auto !important;
+  }
 
-	.show > .tooltip {
-		display: block;
-	}
+  .show > .tooltip {
+    display: block;
+  }
 
-	.host {
-		/* display: flex; */
-	}
+  .host {
+    /* display: flex; */
+  }
 
-	.host .text {
-		display: flex;
-		gap: 8px;
-	}
+  .host .text {
+    display: flex;
+    gap: 8px;
+  }
 
-	.host .label {
-		display: flex;
-	}
+  .host .label {
+    display: flex;
+  }
 
-	.host .content {
-		display: none;
-		position: absolute;
-	}
+  .host .content {
+    display: none;
+    position: absolute;
+  }
 
-	.host.show .content {
-		display: block;
-		z-index: 100;
-	}
+  .host.show .content {
+    display: block;
+    z-index: 100;
+  }
 
-	/* expandOnHover */
+  /* expandOnHover */
 
-	.host.expandOnHover:hover {
-		flex-grow: 1;
-	}
+  .host.expandOnHover:hover {
+    flex-grow: 1;
+  }
 
-	.host.expandOnHover:hover .label .text {
-		flex-grow: 1;
-	}
+  .host.expandOnHover:hover .label .text {
+    flex-grow: 1;
+  }
 </style>
--- Field/index.svelte
+++ Field/index.svelte
@@ -1,171 +1,207 @@
 <script>
-	// import { getContext, createEventDispatcher } from "svelte"
-	// import { valueStore } from "./data.js"
-	// import { onMount } from "svelte"
+  // import { getContext, createEventDispatcher } from "svelte"
+  // import { valueStore } from "./data.js"
+  // import { onMount } from "svelte"
 
-	import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from "svelte";
 
-	const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-	export let placeholder = ""
-	export let value = ""
-	export let label = ""
-	export let disabled = false
-	export let id
-	export let type
-	export let min
-	export let max
-	export let step
-	export let classes = ""
-	export let style
-	export let opts
-	export let icon
-	export let layout
+  export let placeholder = "";
+  export let value = "";
+  export let label = "";
+  export let disabled = false;
+  export let id;
+  export let type;
+  export let min;
+  export let max;
+  export let step;
+  export let classes = "";
+  export let style;
+  export let opts;
+  export let icon;
+  export let layout;
+  export let fontWeight;
 
-	function typeAction(node) {
-		node.type = type;
-	}
-	
+  function typeAction(node) {
+    node.type = type;
+  }
 </script>
 
-<div style={style} class="Field layout-{layout} type-{type}">
-	
-	<label class="{classes}">
-		{#if layout === "stacked" || layout === "inline"}
-			<span class="label">{label}</span>
-		{/if}
-		<div class="input">
-			{#if type === "area"}
-			<textarea
-			on:input
-			rows="4"
-			{id}
-			{disabled}
-			bind:value
-			placeholder={layout !== "stacked" ? placeholder : ""}></textarea>
-			{:else}
-			<input on:input on:focus use:typeAction autocomplete="false"
-				{id}
-				{disabled}
-				placeholder={layout !== "stacked" ? placeholder : ""}
-				bind:value />
-			{/if}
-		</div>
-	</label>
+<div {style} class="Field layout-{layout} type-{type} font-weight-{fontWeight}">
+  <label class={classes}>
+    {#if layout === "stacked" || layout === "inline"}
+      <span class="label">{label}</span>
+    {/if}
+    <div class="input layout-{layout}" data-value={value}>
+      {#if type === "area"}
+        <textarea
+          on:input
+          rows="4"
+          {id}
+          {disabled}
+          bind:value
+          placeholder={layout !== "stacked" ? placeholder : ""}
+        />
+      {:else}
+        <input
+          size="16"
+          on:input
+          on:focus
+          use:typeAction
+          autocomplete="false"
+          {id}
+          {disabled}
+          placeholder={layout !== "stacked" ? placeholder : ""}
+          bind:value
+        />
+      {/if}
+    </div>
+  </label>
 </div>
 
 <style>
+  .font-weight-bold {
+    font-weight: 600;
+  }
+  .layout-stacked .input,
+  .layout-inline .input {
+    border: 1px solid var(--figma-color-border);
+  }
 
-	.layout-stacked .input, .layout-inline .input {
-		border: 1px solid var(--figma-color-border);
-	}
-
-	div {
-		padding-block: 2px;
-	}
-	.input {
-		display: flex;
-		border: 1px solid transparent;
-		place-items: center;
-		height: 28px;
-		/* margin-left: calc(
+  div {
+    padding-block: 2px;
+  }
+  .input {
+    display: flex;
+    border: 1px solid transparent;
+    place-items: center;
+    height: 28px;
+    /* margin-left: calc(
 			var(--fgp-gap_item_column, 0px) + (-1 * var(--margin-100))
 		); */
-		/* margin-right: calc((-1 * var(--margin-100))); */
-		padding-inline: 7px;
-		border-radius: var(--border-radius-25);
-	}
+    /* margin-right: calc((-1 * var(--margin-100))); */
+    padding-inline: 7px;
+    border-radius: var(--border-radius-25);
+  }
 
-	.input:hover {
-		border-color: var(--figma-color-border, var(--color-black-10));
-		border-width: 1px;
-		padding-inline: 7px;
-	}
+  .input:hover {
+    border-color: var(--figma-color-border, var(--color-black-10));
+    border-width: 1px;
+    padding-inline: 7px;
+  }
 
-	.input:focus-within {
-		border-color: var(--figma-color-border-selected, var(--color-blue));
-		border-width: 2px;
-		padding-inline: 6px;
-		padding-block: 1px;
-	}
+  .input:focus-within {
+    border-color: var(--figma-color-border-selected, var(--color-blue));
+    border-width: 2px;
+    padding-inline: 6px;
+    padding-block: 1px;
+  }
 
-	.input {
-		flex-basis: 100%;
-	}
+  .input {
+    flex-basis: 100%;
+  }
 
-	.input span {
-		/* margin-right: var(--margin-200); */
-		color: var(--figma-color-text-tertiary, var(--color-black-30));
-		min-width: 32px;
-		text-align: center;
-		margin-left: -8px;
-	}
+  .input span {
+    /* margin-right: var(--margin-200); */
+    color: var(--figma-color-text-tertiary, var(--color-black-30));
+    min-width: 32px;
+    text-align: center;
+    margin-left: -8px;
+  }
 
-	.input input {
-		flex-grow: 1;
-		cursor: default;
-		width: 50px;
-	}
+  .input input {
+    flex-grow: 1;
+    cursor: default;
+    width: 50px;
+  }
 
-	.label {
-		display: block;
-		margin-bottom: 4px;
-		font-weight: 500;
-	}
+  .label {
+    display: block;
+    margin-bottom: 4px;
+    font-weight: 500;
+  }
 
-	input[disabled] {
-		color: var(--figma-color-text-disabled);
-	}
+  input[disabled] {
+    color: var(--figma-color-text-disabled);
+  }
 
-	.layout-inline label {
-		display: flex;
-		/* align-items: center; */
-	}
+  .layout-inline label {
+    display: flex;
+    /* align-items: center; */
+  }
 
-	.layout-inline .label {
-		margin: 0;
-		margin-top: 7px; /* 6 + 1 to account for border */
-	}
+  .layout-inline .label {
+    margin: 0;
+    margin-top: 7px; /* 6 + 1 to account for border */
+  }
 
-	.layout-inline label > :first-child {
-		flex-basis: 80px;
-		flex-shrink: 0;
-	}
+  .layout-inline label > :first-child {
+    flex-basis: 80px;
+    flex-shrink: 0;
+  }
 
-	.layout-inline label > :last-child {
-		flex-grow: 1;
-	}
+  .layout-inline label > :last-child {
+    flex-grow: 1;
+  }
 
-	/* Type: area */
+  /* Type: area */
 
-	.type-area .input {
-		height: auto;
-		padding: 0;
-	}
+  .type-area .input {
+    height: auto;
+    padding: 0;
+  }
 
-	.type-area textarea {
-		padding: 6px;
-	}
+  .type-area textarea {
+    padding: 6px;
+  }
 
-	.type-area textarea:focus-within {
-		padding: 5px;
-	}
+  .type-area textarea:focus-within {
+    padding: 5px;
+  }
 
-	.type-area textarea {
-		border: none;
-		width: 100%;
-		background-color: transparent;
-		font: inherit;
-		resize: none;
-		cursor: default;
-	}
+  .type-area textarea {
+    border: none;
+    width: 100%;
+    background-color: transparent;
+    font: inherit;
+    resize: none;
+    cursor: default;
+  }
 
-	.type-area textarea:focus-visible {
-		/* border: none; */
-		outline: none;
-	}
+  .type-area textarea:focus-visible {
+    /* border: none; */
+    outline: none;
+  }
 
-	textarea {
-		color: var(--figma-color-text)
-	}
+  textarea {
+    color: var(--figma-color-text);
+  }
+
+  .layout-grow .input {
+    place-items: inherit !important;
+
+    display: inline-grid;
+    vertical-align: top;
+    align-items: center;
+
+    box-sizing: border-box;
+  }
+  .layout-grow .input input {
+    width: auto;
+    min-width: 1em;
+    grid-area: 1/2;
+  }
+  .layout-grow .input::after {
+    content: attr(data-value) " ";
+    visibility: hidden;
+    white-space: pre-wrap;
+
+    width: auto;
+    min-width: 1em;
+    grid-area: 1/2;
+
+    min-height: 1em;
+    /* background-color: rgba(0, 0, 0, 0.1); */
+  }
 </style>
