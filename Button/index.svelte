90ae9e0583e38492f399056eb9bb4afd0e14dcc8
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2023-06-05 00:37:08 +0100

    Added tooltip for iconbutton

--- Button/index.svelte
+++ Button/index.svelte
@@ -1,103 +1,119 @@
 <script>
-	import { getContext, createEventDispatcher } from "svelte"
-	import "../global.css"
-	export const name = "Button"
+  import { getContext, createEventDispatcher } from "svelte";
+  import "../global.css";
+  export const name = "Button";
 
-	export let id
-	export let classes = ""
-	export let label = ""
-	export let icon
-	export let iconRight
-	export let type
-
+  export let id;
+  export let classes = "";
+  export let label = "";
+  export let icon;
+  export let iconRight;
+  export let type;
 </script>
 
-
-	<button on:preventDefault on:click
-	on:mouseover
-	on:mouseenter
-	on:mouseleave
-	on:focus {id} class="Button {classes}" label="{label}" {type}>
-		<div class="gap">
-		{#if icon}<span class="icon" icon={icon} />{/if}<span><slot /></span>{#if iconRight}<span class="icon" icon={iconRight} />{/if}
-		</div>
-	</button>
-
+<button
+  on:preventDefault
+  on:click
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+  on:focus
+  {id}
+  class="Button {classes}"
+  {label}
+  {type}
+>
+  <div class="gap">
+    {#if icon}<span class="icon" {icon} />{/if}<span><slot /></span
+    >{#if iconRight}<span class="icon" icon={iconRight} />{/if}
+  </div>
+</button>
 
 <style>
-	.Button,
-	button {
-		line-height: 22px;
-		padding: var(--padding-0) 11px;
-		border: 2px solid var(--figma-color-bg-brand, var(--color-blue));
-		background-color: var(--figma-color-bg-brand, var(--color-blue));
-		color: white;
-		border-radius: var(--border-radius-75);
-		font-weight: 500;
-		letter-spacing: 0.055px;
-		overflow: hidden;
-	}
+  .Button,
+  button {
+    line-height: 22px;
+    padding: var(--padding-0) 11px;
+    border: 2px solid var(--figma-color-bg-brand, var(--color-blue));
+    background-color: var(--figma-color-bg-brand, var(--color-blue));
+    color: white;
+    border-radius: var(--border-radius-75);
+    font-weight: 500;
+    letter-spacing: 0.055px;
+    overflow: hidden;
+  }
 
-	.Button {
-		border-radius: var(--border-radius-75);
-		display: flex;
-		place-items: center;
-	}
+  .Button {
+    border-radius: var(--border-radius-75);
+    display: flex;
+    place-items: center;
+  }
 
-	.gap {
-		gap: var(--size-50);
-		display: flex;
-		padding-block: 2px !important;
-		place-items: center;
-	}
+  .gap {
+    gap: var(--size-50);
+    display: flex;
+    padding-block: 2px !important;
+    place-items: center;
+  }
 
-	.icon {
-		display: inline-block;
-		width: 24px;
-		height: 24px;
-	}
+  .icon {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+  }
 
-	.secondary {
-		line-height: 24px;
-		background-color: transparent;
-		color: var(--figma-color-text, black);
-		border: 1px solid var(--figma-color-border-strong, white);
-	}
+  .secondary {
+    line-height: 24px;
+    background-color: transparent;
+    color: var(--figma-color-text, black);
+    border: 1px solid var(--figma-color-border-strong, white);
+  }
 
-	.ghost {
-		line-height: 24px;
-		background-color: transparent;
-		color: var(--figma-color-text, black);
-		border: 1px solid transparent;
-	}
+  .ghost {
+    line-height: 24px;
+    background-color: transparent;
+    color: var(--figma-color-text, black);
+    border: 1px solid transparent;
+  }
 
-	.ghost:hover {
-		background-color: var(--figma-color-bg-hover);
-	}
+  .ghost:hover {
+    background-color: var(--figma-color-bg-hover);
+  }
 
-	.tertiary {
-		line-height: 24px;
-		background-color: transparent;
-		color: var(--figma-color-text, black);
-		border: 1px solid transparent;
-	}
+  .tertiary {
+    line-height: 24px;
+    background-color: transparent;
+    color: var(--figma-color-text, black);
+    border: 1px solid transparent;
+  }
 
-	.danger {
-		line-height: 24px;
-		background-color: transparent;
-		color: var(--figma-color-text-danger);
-		border: 1px solid var(--figma-color-border-ondanger);
-	}
+  .danger {
+    line-height: 24px;
+    background-color: transparent;
+    color: var(--figma-color-text-danger);
+    border: 1px solid var(--figma-color-border-ondanger);
+  }
 
-	.tertiary2 {
-		line-height: 24px;
-		background-color: var(--figma-color-bg-tertiary);
-		color: var(--figma-color-text, black);
-		border: 1px solid transparent;
-	}
+  .tertiary2 {
+    line-height: 24px;
+    background-color: var(--figma-color-bg-tertiary);
+    color: var(--figma-color-text, black);
+    border: 1px solid transparent;
+  }
 
-	.Button.small {
-		line-height: 20px;
-		padding: var(--padding-0) 8px;
-	}
+  .Button.small {
+    line-height: 20px;
+    padding: var(--padding-0) 8px;
+  }
+
+  .Button:enabled:active,
+  .Button:enabled:focus {
+    padding: 0 10px;
+    border: 2px solid var(--figma-color-border-selected);
+  }
+
+  .Button:enabled:active .gap,
+  .Button:enabled:focus .gap {
+    padding-block: 1px !important;
+  }
 </style>
--- IconButton/index.svelte
+++ IconButton/index.svelte
@@ -1,62 +1,64 @@
 <script>
-	import { getContext, createEventDispatcher } from "svelte"
-	import "../global.css"
-	export const name = "Button"
+  import { getContext, createEventDispatcher } from "svelte";
+  import "../global.css";
+  export const name = "Button";
 
-	export let id
-	export let classes = ""
-	export let label = ""
-
+  export let id;
+  export let classes = "";
+  export let label = "";
+  export let title;
 </script>
 
-
-	<button on:click
-	on:mouseover
-	on:mouseenter
-	on:mouseleave
-	on:focus {id} class="IconButton {classes}" label="{label}">
-		<div class="contents">
-			<slot />
-		</div>
-	</button>
-
+<button
+  on:click
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+  on:focus
+  {id}
+  class="IconButton {classes}"
+  {label}
+  {title}
+>
+  <div class="contents">
+    <slot />
+  </div>
+</button>
 
 <style>
-	.IconButton {
-		min-width: 32px;
-    	height: 32px;
-		line-height: 32px;
-    	border: 1px solid transparent;
-		background-clip: padding-box;
-		position: relative;
-		border-radius: 3px;
-		/* margin: 2px -3px; */
-		box-sizing: border-box;
-	}
+  .IconButton {
+    min-width: 32px;
+    height: 32px;
+    line-height: 32px;
+    border: 1px solid transparent;
+    background-clip: padding-box;
+    position: relative;
+    border-radius: 3px;
+    /* margin: 2px -3px; */
+    box-sizing: border-box;
+  }
 
-	.IconButton > .contents {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		pointer-events: none;
-		transform: translate(-50%,-50%);
-	}
+  .IconButton > .contents {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    pointer-events: none;
+    transform: translate(-50%, -50%);
+  }
 
-	.IconButton:hover {
-		background-color: var(--figma-color-bg-hover);
-		/* Need this so that when other elements overlap, the one currently being hovered over stays on top */
-		z-index: 100;
-	}
+  .IconButton:hover {
+    background-color: var(--figma-color-bg-hover);
+    /* Need this so that when other elements overlap, the one currently being hovered over stays on top */
+    z-index: 100;
+  }
 
-
-
-	/* .secondary {
+  /* .secondary {
 		background-color: transparent;
 		color: var(--figma-color-text, black);
 		border: 1px solid var(--figma-color-border-strong, white);
 	} */
 
-	/* .tertiary {
+  /* .tertiary {
 		background-color: transparent;
 		color: var(--figma-color-text, black);
 		border: 1px solid transparent;
